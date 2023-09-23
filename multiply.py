7092eec79233dd576c9b40a17bd7497e0fd8e944
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2023-09-23 11:08:45 -0500

    Update multiply.py
    trying to multiply arbritrary numbers

--- multiply.py
+++ multiply.py
@@ -2,6 +2,7 @@ import spidev
 import time
 from gpiozero import LED
 from smbus import SMBus
+from math import log
 
 address = 0x00
 STEPS = 128
@@ -53,11 +54,28 @@ def multiply(a,b):
   digitalPotWrite(0, steps_r1)
   digitalPotWrite(1, steps_r2)
   return read(0)
-  
+
+I_interval = [0.001, 0.00009] #in A
+R_interval = [10**5, 100]
+
 def multiply(a,b):
   if a == 0 or b == 0: raise Exception("no 0!!")
-  R_eq = (5000)/b #dividing 5/b would be too small to put on the digipots and would assume a too large of a current
+  
+  # the amount to multiply b by to make it in the I_interval
+  b_factor = -int(log(a, 10)) - 4
+  mb *= 10**b_factor
+  if mb > I_interval[0]:
+     mb *= 10
+     #b_factor += 1
+
+  if mb < I_interval[1]:
+     raise Exception(f"Cannot put {b} into the Current Interval: {mb}")
+      
+  
+  R_eq = (5)/mb 
   R1 = a # just A
+  r_factor = 4 - log(a, 10)
+  R1 *= r_factor
   R2 = R_eq - R1 # need to have this value so that the total R_eq stays the same
   steps_r1 = to_steps(R1)
   steps_r2 = to_steps(R2)
@@ -70,9 +88,4 @@ def error(a,b):
   v = multiply(a,b)
   return (v-true_value)/true_value
 
-data = {}
-for a in range(1, 120):
-  for b in range(1, 9):
-    data[a] = data.get(a, []) + [error(a*88, b)]
 
-print(data)
