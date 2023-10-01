181b664d200e0e1d9418845c51bb5797dbb2df89
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2023-10-01 14:36:33 -0500

    made the AI work

--- AI/predict.py
+++ AI/predict.py
@@ -1,46 +1,134 @@
 import numpy as np
+import h5py 
+from tensorflow.keras.datasets import mnist
 
-weights = [[]]
+# Load the dataset into training and testing data
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+f = h5py.File('mnist_weightsh.h5', 'r')
+
+
+weights = [f['dense']['dense'],
+           f['dense_1']['dense_1']]
 weights_nueron_num = [0]*len(weights)
-for i,layer in enumerate(weights):
-    weights_nueron_num[i] = len(layer)
-
-last_layer_output = []
-
-
 
 data = 1
 hidden_layers = 2
 def predict(data):
-    last_layer_output = data
-    for layer in range(hidden_layers):
-        out_data = []
 
-        for nueron in weights[layer]:
-            out_data.append( relu(dot_pb(nueron, last_layer_output)) )
+    last_layer_output = data.flatten()
+    for layer in range(hidden_layers):
+        last_layer_output = ( relu(dot_pb(weights[layer], last_layer_output)) )
         
-        last_layer_output = out_data
+        
+        #print(last_layer_output)
+        #print(len(out_data[0]))
     #now all of the hidden layers have been computed
     # and now we have to computer the output layer
 
     probs = softmax(last_layer_output)
-    return probs.index(max(probs))
+    return np.argmax(probs)
 
 
 
 def softmax(i):
-    return np.exp(xs) / sum(np.exp(xs))
+    return np.exp(i) / sum(np.exp(i))
 
-def relu():
+def relu(f):
     return f*(f>0)
 
-def dot_pb(wb, i):
+def dot_pb(wb, input):
     #wb[0] is the weights matrix
     #wb[1] is just the bias value
-    sum = wb[1]
-    for weight in wb[0]:
-        sum += multiply(weight, i)
+    #print(type(wb))
+    #print(wb['kernel:0'].shape)
+    #print(len(input))
+    sum = dot(wb['kernel:0'], input)
+    return np.array(sum) + np.array(wb['bias:0'])
+
+def dot(vector_a, vector_b):
+    return sum(a * b for a, b in zip(vector_a, vector_b))   
 
 def multiply(a, b):
     return a*b
 
+correct = 0
+nums = {
+}
+
+total = 1000 #60k
+for i, image in enumerate(train_images[:total]):
+    prediction = predict(image)
+    label = train_labels[i]
+    if prediction == label:
+        correct += 1 
+    else:
+        nums[str(prediction) + " " + str(label)] = nums.get(prediction, 0) + 1
+
+print(correct/total)
+
+print(nums)
+
+ex = np.array([  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   3,  18,  18,  18,
+       126, 136, 175,  26, 166, 255, 247, 127,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,  30,  36,  94, 154, 170, 253,
+       253, 253, 253, 253, 225, 172, 253, 242, 195,  64,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,  49, 238, 253, 253, 253,
+       253, 253, 253, 253, 253, 251,  93,  82,  82,  56,  39,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  18, 219, 253,
+       253, 253, 253, 253, 198, 182, 247, 241,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+        80, 156, 107, 253, 253, 205,  11,   0,  43, 154,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,  14,   1, 154, 253,  90,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0, 139, 253, 190,   2,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,  11, 190, 253,  70,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  35,
+       241, 225, 160, 108,   1,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,  81, 240, 253, 253, 119,  25,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,  45, 186, 253, 253, 150,  27,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,  16,  93, 252, 253, 187,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, 249,
+       253, 249,  64,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  46, 130,
+       183, 253, 253, 207,   2,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  39, 148,
+       229, 253, 253, 253, 250, 182,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  24, 114,
+       221, 253, 253, 253, 253, 201,  78,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  23,  66,
+       213, 253, 253, 253, 253, 198,  81,   2,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  18, 171,
+       219, 253, 253, 253, 253, 195,  80,   9,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  55, 172,
+       226, 253, 253, 253, 253, 244, 133,  11,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       136, 253, 253, 253, 212, 135, 132,  16,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0], dtype=np.uint8)
+
+print(predict(ex))
